c02288889571358d1120ed3338bcdf5771674cb6
Jeremy Pike <PikeJA@adf.bham.ac.uk>
2017-06-19 17:36:48 +0200

    comments and user input added

--- src/main/java/uk/ac/birmingham/LLSPreprocess.java
+++ src/main/java/uk/ac/birmingham/LLSPreprocess.java
@@ -13,7 +13,6 @@ import org.scijava.plugin.Plugin;
 import org.scijava.ui.UIService;
 
 import net.imagej.Dataset;
-import net.imagej.DatasetService;
 import net.imagej.ImageJ;
 import net.imagej.ops.OpService;
 import net.imglib2.FinalInterval;
@@ -54,11 +53,17 @@ public class LLSPreprocess<T extends RealType<T>> implements Command {
 	@Parameter
 	private OpService opService;
 
-	@Parameter
-	private DatasetService datasetService;
-
 	@Parameter(label = "Select the image file for the PSF ", persist = true, style = "FILE")
 	private File psfFile;
+
+	@Parameter(label = "Define the shear for each slice (pixels): ", persist = false, min = "0.", max = "100.")
+	private double shearPix = 1.;
+
+	@Parameter(label = "Define the rotation angle (degrees): ", persist = false, min = "0.", max = "90.")
+	private double angleDegrees = 32.8;
+
+	@Parameter(label = "Maximum number of itearions foe deconvolution: ", persist = false, min = "1", max = "200")
+	private int maxItDecon = 1;
 
 	@Parameter(type = ItemIO.OUTPUT)
 	private IntervalView<FloatType> result;
@@ -67,37 +72,42 @@ public class LLSPreprocess<T extends RealType<T>> implements Command {
 	public void run() {
 		final Img<T> image = (Img<T>) currentData.getImgPlus();
 
-		// shear the image
-		final AffineTransform3D affine = new AffineTransform3D();
-		// final AffineTransform2D affine = new AffineTransform2D();
-		affine.set(new double[][] { { 1., 0., 1., 0. }, { 0., 1., 0., 0. }, { 0., 0., 1., 0. }, { 0., 0., 0., 1. } });
-		// affine.set(new double[][] { { 1., 1., 0.}, { 0., 1., 0.}, { 0., 0.,
-		// 1.} });
-
-		IntervalView<T> bounded = affineBoundingBox(image, affine, new NLinearInterpolatorFactory<>());
-		// long[] borderSize = new long[bounded.numDimensions()];
-		// Arrays.fill(borderSize, 2);
-
+		// create affine transform for shear
+		final AffineTransform3D affineShear = new AffineTransform3D();
+		affineShear.set(new double[][] { { 1., 0., shearPix, 0. }, { 0., 1., 0., 0. }, { 0., 0., 1., 0. },
+				{ 0., 0., 0., 1. } });
+		
+		// apply affine transform and retrieve bounded view
+		IntervalView<T> deskewed = affineBoundingBox(image, affineShear, new NLinearInterpolatorFactory<>());
+		
+		
 		try {
+			// open the psf stack
 			Img<T> psf = (Img<T>) ioService.open(psfFile.getAbsolutePath());
-			uiService.show(psf);
-
-			// deconvolved = opService.deconvolve().richardsonLucy(bounded, psf,
-			// 5);
-			Img<FloatType> boundedFloat = opService.convert().float32(bounded);
+			
+			// convert both deskewed View and psf to Float
+			// this seems to be needed to get the deconvolution op to work????
+			Img<FloatType> deskewedFloat = opService.convert().float32(deskewed);
 			Img<FloatType> psfFloat = opService.convert().float32(psf);
+			
+			// deconvolve with standard RL algorithm
 			RandomAccessibleInterval<FloatType> deconvolved = (RandomAccessibleInterval<FloatType>) opService
-					.run("richardsonLucy", boundedFloat, psfFloat, 1);
-			uiService.show(deconvolved);
-
+					.run("richardsonLucy", deskewedFloat, psfFloat, maxItDecon);
+			
+			// convert rotation angle to radians
+			final double angleRad = Math.toRadians(angleDegrees);
+		
+			// create affine transform for rotation
 			final AffineTransform3D affineRot = new AffineTransform3D();
-			affineRot.set(new double[][] { { 0.8660254038, 0., 0.5, 0. }, { 0., 1., 0., 0. },
-					{ -0.5, 0., 0.8660254038, 0. }, { 0., 0., 0., 1. } });
+			affineRot.set(new double[][] { { angleRad, 0., 0.5, 0. }, { 0., 1., 0., 0. }, { -0.5, 0., angleRad, 0. },
+					{ 0., 0., 0., 1. } });
+			// apply affine transform and retrieve bounded view
 			IntervalView<FloatType> rotated = affineBoundingBox(deconvolved, affineRot,
 					new NLinearInterpolatorFactory<>());
+			// Redefine coordinate system so that the image can be displayed despite the possible negative Interval
+			// This is necessary because of a bug????
 			result = Views.zeroMin(rotated);
-			// uiService.show(rotated);
-
+			
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -105,33 +115,60 @@ public class LLSPreprocess<T extends RealType<T>> implements Command {
 
 	}
 
+	
+	/**
+	 * The function performs an arbitrary affine transform for an arbitrary number of dimensions.
+	 * The resulting view is automatically bounded
+	 * 
+	 * @param image input image
+	 * @param affine defines the transformation
+	 * @param interpolator defines the type of interpolation
+	 * 
+	 * @return the transformed data defined on a View with bounded source
+	 */
 	private <K extends RealType<K>> IntervalView<K> affineBoundingBox(final RandomAccessibleInterval<K> image,
 			final AffineGet affine, InterpolatorFactory<K, RandomAccessible<K>> interpolator) {
-
+		
+		// extend with zeros
 		final RealRandomAccessible<K> field = Views.interpolate(Views.extendZero(image), interpolator);
+		
+		// transform the data
 		final AffineRandomAccessible<K, AffineGet> sheared = RealViews.affine(field, affine);
+		
 		int numDims = sheared.numDimensions();
+		
+		// to hold the bounds of the transformed data
 		long[] min = new long[numDims];
 		long[] max = new long[numDims];
 		Arrays.fill(min, Long.MAX_VALUE);
 		Arrays.fill(max, Long.MIN_VALUE);
-
+		
+		// to hold the corners of the dataset before and after transformation
+		final double[] source = new double[numDims];
 		double[] target = new double[numDims];
-
+		
+		// dataset dimensions
 		final long[] dims = new long[numDims];
 		image.dimensions(dims);
-
+		
+		// for calculating the corder coordinates
 		final long[] normDims = new long[numDims];
 		Arrays.fill(normDims, 2);
+		
+		// to hold unnormalised coordinates, eg [1, 0, 0] or [1, 1, 1]
 		final long[] normPos = new long[numDims];
-		final double[] source = new double[numDims];
+		
+		// loop over corners, there are 2^(numDimensions)
 		for (int i = 0; i < Math.pow(2, numDims); i++) {
+			// calculate unnormalised coordinates
 			IntervalIndexer.indexToPosition(i, normDims, normPos);
+			// normalise coordinate
 			for (int d = 0; d < numDims; d++)
 				source[d] = dims[d] * normPos[d];
-
+			// apply affine transform to corner
 			affine.apply(source, target);
-
+			
+			// if corner is either current extremal value in any dimension update bounds
 			for (int d = 0; d < min.length; d++) {
 				if (target[d] < min[d])
 					min[d] = (long) Math.floor(target[d]);
@@ -141,10 +178,10 @@ public class LLSPreprocess<T extends RealType<T>> implements Command {
 
 			}
 		}
-		for (int d = 0; d < min.length; d++) {
-			System.out.println(d + " min:" + min[d] + " max:" + max[d]);
-		}
+		// create interval from extremal values
 		FinalInterval bounds = new FinalInterval(min, max);
+		
+		// return View with bounded source
 		return Views.interval(sheared, bounds);
 
 	}
@@ -164,17 +201,20 @@ public class LLSPreprocess<T extends RealType<T>> implements Command {
 
 		// load example dataset
 		Dataset dataset = (Dataset) ij.io().open("testStack.tif");
-
+		
+		// crop the dataset so it works on my laptop. This is a bit messy...
 		FinalInterval interval = FinalInterval.createMinSize(0, 0, 0, dataset.dimension(0), 400, 30);
 		RandomAccessibleInterval<ShortType> croppedAcceesible = (RandomAccessibleInterval<ShortType>) ij.op()
 				.run("crop", dataset, interval, true);
 		dataset = null;
-		// show the image
+		// show the cropped image
 		ij.ui().show(croppedAcceesible);
 
 		// invoke the plugin
 		ij.command().run(LLSPreprocess.class, true);
 
 	}
+	
+
 
 }
